9fe8e9bce38a88d37e5f3339c29255370854dcb0
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-06-18 18:26:03 +0530

    Update q9.java

--- DSA/Arrays/Easy/q9.java
+++ DSA/Arrays/Easy/q9.java
@@ -45,4 +45,19 @@ class q10 {
     }
 }
 
+// XOR METHOD (optimal-est method)
+public static int missingNumber(int []a, int N) {
+
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            xor2 = xor2 ^ a[i]; // XOR of array elements
+            xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+        }
+        xor1 = xor1 ^ N; //XOR up to [1...N]
+
+        return (xor1 ^ xor2); // the missing number
+    }
+
+
     

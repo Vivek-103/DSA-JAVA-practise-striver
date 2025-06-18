4d5bcd2b0ce3777532d881f52e3cc5a299d62349
Vivek <168767894+Vivek-103@users.noreply.github.com>
2025-06-18 19:25:15 +0530

    Create q02.java

---
+++ DSA/Arrays/Medium/q02.java
@@ -0,0 +1,72 @@
+// finding number that appears one time in an ARRAY where other appears TWICE
+
+import java.util.HashMap;
+import java.util.Map;
+
+class q2 {
+    //BRUTE FORCE METHOD
+    public static int getSingleElement(int []arr) {
+        // Size of the array:
+        int n = arr.length;
+
+        //Run a loop for selecting elements:
+        for (int i = 0; i < n; i++) {
+            int num = arr[i]; // selected element
+            int cnt = 0;
+
+            //find the occurrence using linear search:
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == num)
+                    cnt++;
+            }
+
+            // if the occurrence is 1 return ans:
+            if (cnt == 1) return num;
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    // BETTER APPROACH USING HASH MAPS
+    public static int getSingleElementBetter(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // Declare the hashmap.
+        // And hash the given array:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(arr[i], 0);
+            mpp.put(arr[i], value + 1);
+        }
+
+        //Find the single element and return the answer:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() == 1) {
+                return it.getKey();
+            }
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+        //OPTIMAL APPROACH
+         public static int getSingleElementOptimal(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ arr[i];
+        }
+        return xorr;
+    }
+
+
+
+    
+}
